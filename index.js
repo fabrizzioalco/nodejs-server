4dd3ee61bb5a265d35ec4790206e5af84eb7f098
Fabrizzio <fabrizzio166@gmail.com>
2019-08-23 12:42:58 -0500

    New post and get

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require("express");
 
+//librerias necesarias 
+const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
@@ -9,12 +11,12 @@ let error = {
   codigo: 200,
   mensaje: ''
 };
-
+//definimos objetos de usuario
 let user ={
     nombre:'',
     apellido:''
 };
-
+//get method in root 
 app.get('/', function(req, res){
     respuesta = {
         error:true,
@@ -22,6 +24,50 @@ app.get('/', function(req, res){
         mensaje: 'Punto de inicio'
 
     };
+    res.send(respuesta);
+});
+
+
+//Consultamos la base de datos verficando que no sea notacion SQL, asi evitamos ataque SQL injection
+
+// TODO añadir verificacionde texto que no sea SQL
+
+app.get('/usuario', function(req, res){
+    //Verificamos que el usuario exista
+if(user.nombre== '' && user.apellido == ''){
+    respuesta = {
+        error: true,
+        codigo: 501,
+        mensaje: 'El usuario no existe'
+    }
+}else{
+    respuesta= {
+        error: false,
+        codigo: 200,
+        mensaje: "Usuario encontrado",
+        
+
+    }
+    respuesta = user;
+}
+res.send(respuesta);
+});
+//Post 
+app.post('/userRegistration', function(req, res){
+    if(!req.user.nombre || !req.user.apellido){
+        respuesta= {
+            error: true,
+            codigo: 503,
+            mensaje: "Campos requeridos"
+        };
+
+    }else{
+        respuesta ={
+            error: false,
+            codigo: 200,
+            mensaje: "Loggeado con exito"
+        };
+    }
     res.send(respuesta);
 });
 
